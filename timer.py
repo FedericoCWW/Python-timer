9a644dfbfae17636a6128bcf5eed0927ef0ba8d3
Federico <133919977+FedericoCWW@users.noreply.github.com>
2023-08-11 16:15:40 -0300

    second-commit

--- timer.py
+++ timer.py
@@ -2,10 +2,34 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 
+#functions
+def submit():
+    try:       #exception check
+        temp = (int(hour.get())*3600) + (int(minute.get())* 60) + (int(second.get()))
+    except:
+        print("There is no value inputed")
+    while temp > -1:
+        mins,secs = divmod(temp, 60)     #converts minutes or seconds entered to hours
+        hours = 0
+        if mins > 60:
+            hours, mins = divmod(mins, 60)
+        hour.set("{0:2d}".format(hours))
+        minute.set("{0:2d}".format(mins))
+        second.set("{0:2d}".format(secs))
+
+        root.update()
+        time.sleep(1)
+
+        if temp == 0:
+            messagebox.showinfo(title="Time's up!", message="The timer countdown has finished")
+        
+        temp -= 1
+
 root = tk.Tk()
 
 root.title("Timer")
-root.geometry("250x200")
+root.geometry("250x150")
+root.iconbitmap('Python-timer/favicon.ico')
 
 
 hour = tk.StringVar()
@@ -16,19 +40,18 @@ hour.set("00")
 minute.set("00")
 second.set("00")
 
-timeframe = tk.Frame(root)
+hourentry = tk.Entry(root, width=2, font=("Arial, 18"), textvariable=hour)
+hourentry.pack(side='left', expand=True)
+minuteentry = tk.Entry(root, width=2, font=("Arial, 18"), textvariable=minute)
+minuteentry.pack(side='left', expand=True)
+secondentry = tk.Entry(root, width=2, font=("Arial, 18"), textvariable=second)
+secondentry.pack(side='left', expand=True)
 
-hourentry = tk.Entry(timeframe, width=2, font=("Arial, 18"), textvariable=hour)
-hourentry.grid(row=0, column=0)
-minuteentry = tk.Entry(timeframe, width=2, font=("Arial, 18"), textvariable=minute)
-minuteentry.grid(row=0, column=1)
-secondentry = tk.Entry(timeframe, width=2, font=("Arial, 18"), textvariable=second)
-secondentry.grid(row=0, column=2)
-
-timeframe.pack(fill='x')
-
-root.update()
-
-btn = tk.Button(root, text="Start", font=("Arial, 20"))
-btn.pack(pady= 60)
+btn = tk.Button(root, text="Start", font=("Arial, 20") , command=submit)
+btn.pack(expand=True)
 root.mainloop()
+
+
+        
+
+
